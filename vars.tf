678969332dd588f78f667fe6492e36d2300760b7
kavin <kavinsubramaniam2002@gmail.com>
2023-06-04 12:41:49 +0530

    second commit

---
+++ vars.tf
@@ -0,0 +1,13 @@
+variable "AWS_ACCESS_KEY" {}
+variable "AWS_SECRET_KEY" {}
+
+variable "AWS_REGION" {
+    default ="ap-south-1"
+}
+
+//variable "key_name" {}
+variable "vpccidr" {}
+variable "pubsub1cidr" {}
+variable "pubsub2cidr" {}
+variable "prisub1cidr" {}
+variable "prisub2cidr" {}
